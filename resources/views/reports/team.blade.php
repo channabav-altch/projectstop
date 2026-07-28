34ccce1103040695ae06dfa60650f9acf9124003
CHANNA <khdrama37@gmail.com>
2026-07-28 10:29:28 +0700

    អតិថិជនសរុប

--- resources/views/reports/team.blade.php
+++ resources/views/reports/team.blade.php
@@ -169,7 +169,7 @@
                             <th class="px-6 py-5 text-center">តំណាងលក់សរុប</th>
                             <th class="px-6 py-5 text-center">ចំនួនលក់ (UNITS)</th>
                             <td class="px-6 py-4 text-center">
-    <span class="font-bold text-white">{{ $manager->total_customers ?? 0 }}</span> <span class="text-slate-500 text-xs">នាក់</span>
+    <span class="font-bold text-white">{{ $manager->total_customers ?? 0 }}</span> <span class="text-slate-500 text-xs">អតិថិជនសរុប</span>
 </td>
                             <th class="px-6 py-5 text-right">ចំណូល</th>
                         </tr>
